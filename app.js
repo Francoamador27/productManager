7236f6399891238d0f12903a38233ae0b232fc0a
Francoamador27 <francohugoamador@hotmail.com>
2023-05-02 13:04:29 +0200

    Modificacion Limit

--- app.js
+++ app.js
@@ -14,21 +14,18 @@ app.listen(port, () => {
 //CON QUERY  ?ID=
 
 app.get('/products', async (req, res) => {
-    try{
     const limit = req.query.limit;
     const products = await productManager.getProducts();
     let quantity = products.length; 
     if(limit){
                 if(limit <= quantity ){
-                return res.json(products.slice(0,limit))
+                    return res.status(200).json(products.slice(0,limit));
                 }else{
-                    throw res.status(409).json({message:"error No tenemos esa cantidad"});
+                    return res.status(400).json({message:"error No tenemos esa cantidad"});
                 }
             }
-         return res.json(products)
-    }catch(e){
-
-    }
+         return res.status(200).json(products)
+   
 
     });
 
